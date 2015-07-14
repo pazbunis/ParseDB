f97fd97709714e139b10b6d7ab83f23504b84293
Paz Bunis <pazbunis@gmail.com>
2015-07-14 19:47:43 +0300

    Added an example of uploading an image to Parse

--- app/src/main/java/il/ac/huji/parsedb/MainActivity.java
+++ app/src/main/java/il/ac/huji/parsedb/MainActivity.java
@@ -1,10 +1,19 @@
 package il.ac.huji.parsedb;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -13,20 +22,14 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Adding simple rows to a relation:
-        ParseObject gameScore = new ParseObject("Item"); // "Item" is an object class (can be thought of as a table)
-        gameScore.put("score", 1337);
-        gameScore.put("playerName", "Sean Plott");
-        gameScore.put("cheatMode", false);
-        gameScore.saveInBackground();
-
-        gameScore = new ParseObject("Item");
-        gameScore.put("score", 1338);
-        gameScore.put("playerName", "Sean Plott");
-        gameScore.put("cheatMode", false);
-        gameScore.saveInBackground();
-
-
+        final Button btnUpload =(Button) findViewById(R.id.btnUpload);
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addSimpleRowToParse();
+                uploadImageToParse();
+            }
+        });
     }
 
     @Override
@@ -49,5 +52,52 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addSimpleRowToParse(){
+        //Adding simple rows to a relation:
+        ParseObject coolItem= new ParseObject("Item"); // "Item" is an object class (can be thought of as a table)
+        coolItem.put("score", 1337);
+        coolItem.put("playerName", "Sean Plott");
+        coolItem.put("cheatMode", false);
+        coolItem.saveInBackground();
+
+        coolItem = new ParseObject("Item");
+        coolItem.put("score", 1338);
+        coolItem.put("playerName", "Sean Plott");
+        coolItem.put("cheatMode", false);
+        coolItem.saveInBackground();
+    }
+
+    public void uploadImageToParse(){
+        // Locate the image in res > drawable-hdpi
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.droid);
+        // Convert it to byte
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        // Compress image to lower quality scale 1 - 100
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] image = stream.toByteArray();
+
+        // Create the ParseFile
+        ParseFile file = new ParseFile("droid.jpg", image);
+        // Upload the image into Parse Cloud
+        file.saveInBackground();
+
+        // Create a New Class called "ImageUpload" in Parse
+        ParseObject imgupload = new ParseObject("ImageUpload");
+
+        // Create a column named "ImageName" and set the string
+        imgupload.put("ImageName", "AndroidBegin Logo");
+
+        // Create a column named "ImageFile" and insert the image
+        imgupload.put("ImageFile", file);
+
+        // Create the class and the columns
+        imgupload.saveInBackground();
+
+        // Show a simple toast message
+        Toast.makeText(MainActivity.this, "Image Uploaded",
+                Toast.LENGTH_SHORT).show();
     }
 }
